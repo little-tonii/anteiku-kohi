edc4e0012378764a5aa26f8ecd69287ef7c7994f
little-tonii <laptrinhcungtony@gmail.com>
2025-04-24 00:21:19 +0700

    refactor mail config

--- src/application/background_task/send_email_verification.py
+++ src/application/background_task/send_email_verification.py
@@ -1,20 +1,7 @@
-from fastapi_mail import MessageType, FastMail, MessageSchema, ConnectionConfig
-from ...infrastructure.config.variables import ANTEIKU_KOHI_EMAIL, ANTEIKU_KOHI_EMAIL_APP_PASSWORD
-from pydantic import SecretStr
 from ...infrastructure.config.serializer import serializer
 from ...infrastructure.config.variables import EMAIL_SALT_VERIFYCATION
-
-conf = ConnectionConfig(
-    MAIL_USERNAME=ANTEIKU_KOHI_EMAIL,
-    MAIL_PASSWORD=SecretStr(ANTEIKU_KOHI_EMAIL_APP_PASSWORD),
-    MAIL_FROM=ANTEIKU_KOHI_EMAIL,
-    MAIL_FROM_NAME="Anteiku Team",
-    MAIL_PORT=587,
-    MAIL_SERVER="smtp.gmail.com",
-    USE_CREDENTIALS=True,
-    MAIL_SSL_TLS=False,
-    MAIL_STARTTLS=True,
-)
+from fastapi_mail import FastMail, MessageSchema, MessageType
+from ...infrastructure.config.mailing import mail_config
 
 async def send_email_verification(email: str):
     verification_token = serializer.dumps(email, salt=EMAIL_SALT_VERIFYCATION)
@@ -25,5 +12,5 @@ async def send_email_verification(email: str):
         body=f"Liên kết xác thực: <a href=\"{confirmation_url}\">{confirmation_url}</a>",
         subtype=MessageType.html
     )
-    fm = FastMail(conf)
+    fm = FastMail(mail_config)
     await fm.send_message(message)
